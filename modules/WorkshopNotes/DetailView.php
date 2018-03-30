47b5d052a5355c9f085e9fe4e103402970c43e5d
Luke1982 <gcx3@hotmail.com>
2018-03-30 10:39:31 +0200

    Initial commit

---
+++ modules/WorkshopNotes/DetailView.php
@@ -0,0 +1,20 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+require_once('Smarty_setup.php');
+require_once('user_privileges/default_module_view.php');
+
+global $mod_strings, $app_strings, $currentModule, $current_user, $theme, $log, $singlepane_view;
+
+$smarty = new vtigerCRM_Smarty();
+
+require_once 'modules/Vtiger/DetailView.php';
+
+$smarty->display('DetailView.tpl');
+?>
